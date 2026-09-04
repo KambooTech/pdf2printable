93f3659406859374c463505a4bff2d12761f6408
KambooTech <kambootechcore@gmail.com>
2026-09-05 00:35:12 +0530

    chore: initialize PDF 2 PRINTABLE project

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './styles/base.css';
+
+const app = document.querySelector('#app');
+
+app.innerHTML = `
+  <main class="app">
+    <h1>PDF 2 PRINTABLE</h1>
+    <p>Turn Digital Notes into Printable PDFs.</p>
+  </main>
+`;
